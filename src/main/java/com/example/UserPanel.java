e46f0722dea5f1933325b03e1325b790d15afc89
estebann23 <e.naranjo@student.maastrichtuniversity.nl>
2024-10-08 15:56:33 +0200

    Updated UserPanel v1.0
    need to add actual name and info..

--- src/main/java/com/example/UserPanel.java
+++ src/main/java/com/example/UserPanel.java
@@ -9,6 +9,14 @@ public class UserPanel extends JDialog{
     private final PizzaDeliveryApp app;
     private final JLabel pizzasOrdered;
 
+    // UserInfoPanel fields
+    private JTextField nameField = new JTextField();
+    private JTextField genderField = new JTextField();
+    private JTextField birthdateField = new JTextField();
+    private JTextField emailField = new JTextField();
+    private JTextField phoneField = new JTextField();
+
+
     public UserPanel(PizzaDeliveryApp app) {
         super(app.getFrame(), "User information", true);
         this.app = app;
@@ -16,10 +24,27 @@ public class UserPanel extends JDialog{
         initialize();
     }
 
-    private void initialize() {
+    public void initialize() {
         setLayout(new BorderLayout(10, 10));
         setPreferredSize(new Dimension(500, 400));
         setResizable(false);
+
+        // User Info Panel settings
+
+        JPanel userInfoPanel = new JPanel(new GridLayout(9, 2, 10, 10));
+        userInfoPanel.setBorder(new EmptyBorder(10, 20, 10, 20));
+        userInfoPanel.add(new JLabel("Name:"));
+        userInfoPanel.add(nameField);
+        userInfoPanel.add(new JLabel("Gender:"));
+        userInfoPanel.add(genderField);
+        userInfoPanel.add(new JLabel("Birthdate:"));
+        userInfoPanel.add(birthdateField);
+        userInfoPanel.add(new JLabel("Email:"));
+        userInfoPanel.add(emailField);
+        userInfoPanel.add(new JLabel("Phone:"));
+        userInfoPanel.add(phoneField);
+        setUserInfo();
+        add(userInfoPanel, BorderLayout.CENTER);
 
         // Bottom panel: For amount of pizzas ordered
         JPanel bottomPanel = new JPanel(new BorderLayout(10, 10));
@@ -31,7 +56,9 @@ public class UserPanel extends JDialog{
         pack();
         setLocationRelativeTo(app.getFrame());
     }
-
+    public void setUserInfo() {
+        nameField.setText("actual name");
+    }
     public void showTotalPizzasOrdered() {
         String username = app.getCurrentUsername();
         if (username != null) {
